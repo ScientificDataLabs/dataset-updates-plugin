ba5c49b62e9d1737f2da88382146b36b34fca563
tiest <xp3@tiestvilee.fastmail.com.au>
2017-11-29 16:38:40 +0000

    recursively look for dois - so the text is a bit better to parse cos it's slieghtly more semantic

--- plugin/find_dois.js
+++ plugin/find_dois.js
@@ -1,18 +1,24 @@
 // const doiMatcher = /(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?![%"#? ])\\S)+)/
 
 function findDois(node) {
-	const doiMatcher = /10\.[0-9]{4,}(\.[0-9]+)*\/[\S]+/g
-	var dois = []
-	var matches = null;
+	 if (node.nodeType === Node.TEXT_NODE) {
+		const doiMatcher = /10\.[0-9]{4,}(\.[0-9]+)*\/[\S]+/g
+		var dois = []
+		var matches = null;
 
-	do {
-		matches = doiMatcher.exec(node.textContent)
-    	if (matches) {
-    		dois.push(matches[0])
-    	}
-	} while (matches);
-
-	console.log("found dois using regex: ", dois)
-
-	return dois;
+		do {
+			matches = doiMatcher.exec(node.textContent)
+	    	if (matches) {
+	    		dois.push(matches[0])
+	    	}
+		} while (matches);
+		return dois;
+	} else {
+		var dois = []
+		for (let i = 0; i < node.childNodes.length; i++) {
+			const foundDois = findDois(node.childNodes[i])
+      		dois = dois.concat(foundDois)
+		}
+		return dois
+	}
 }
