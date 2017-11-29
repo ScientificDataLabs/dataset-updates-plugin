5db40817aa73f645a1e7c90352d3f9b191ee08b6
David Wickes <dave.wickes@gmail.com>
2017-11-29 13:35:28 +0000

    List of DOI refs from cross ref

--- plugin/rupdate.js
+++ plugin/rupdate.js
@@ -1,5 +1,7 @@
 const doiTag = document.querySelector('meta[name=citation_doi]')
 
+const crossRefAPI = function (doi) { return `http://api.crossref.org/works/${doi}` }
+
 const styleCss = `
 body {
 	border: 5px solid red;
@@ -14,4 +16,36 @@ if (doiTag) {
 	var element = document.createElement('div')
 	element.innerHTML = `<h1>HELLO DOILLY: ${doiTag.content}</h1>`
 	document.body.appendChild(element)
+
+	console.log(crossRefAPI(doiTag.content))
+
+	fetch(crossRefAPI(doiTag.content))
+		.then(response => {
+			console.log(response)
+			return response.json()
+		})
+		.then(object => {
+			const referenceDOIs = new Set(object.message.reference
+				.map(r => r.DOI)
+				.filter(r => r !== undefined))
+			lookUpInFig([...referenceDOIs], (results) => {
+				displayReferences(results, element)
+			})
+
+		})
+		.catch(e => console.error(e))
 }
+
+function lookUpInFig(dois, callback) {
+		// your code here	
+}
+
+function displayReferences(references, element) {
+	console.log(references)
+	const list = references.reduce((list, reference) => {
+		return list + `<li>${reference}</li>`
+	}, "<ul>") + "</ul>"
+	element.innerHTML = list
+}
+
+
